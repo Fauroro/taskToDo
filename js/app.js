73e884d5e37ee25923a18b0875deae25c7686e36
Fauroro <151984072+Fauroro@users.noreply.github.com>
2024-03-04 09:07:11 -0500

    feat: :bug: Fix page Github
    Fix page Github

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
-import './app/navMenu/navMenu.js';
-import './app/createTask/createTask.js';
-import './app/createTask/taskToDo.js';
-import './app/createTask/taskDone.js';
-import './app/createTask/taskFail.js';
-import './app/createTask/taskTrash.js';
+import './../app/navMenu/navMenu.js';
+import './../app/createTask/createTask.js';
+import './../app/createTask/taskToDo.js';
+import './../app/createTask/taskDone.js';
+import './../app/createTask/taskFail.js';
+import './../app/createTask/taskTrash.js';
 
 
 addEventListener("DOMContentLoaded", (event) => {
